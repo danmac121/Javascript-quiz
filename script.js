13471b0ac0174c3ec41ce66cd0dcf24c18213c89
danmac121 <danmacdonald121@gmail.com>
2023-08-01 14:27:13 -0400

    Fixed the high scores page and associated functions, local storage now stores all scores and associated playernames and successfully displays them on screen. I also made is so the scores are sorted into highest to lowest order. Hitting the try again button on the final page refreshes the page, because i could not get calling the initialize function to work.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let timer = 10;
+let timer = 60;
 let timeId;
 let spanTimeout = 5;
 let currentQuestion = 0
@@ -22,9 +22,11 @@ let submit = document.querySelector("#submit")
 let scores = document.querySelector("#highScores")
 let tryAgain = document.querySelector("#tryAgain")
 let highScoreLink = document.querySelector("#highScoreLink")
-
-
-
+let userName = document.getElementById('name').value
+let allScores = document.querySelector("#allScores")
+let nameBox = document.querySelector("#name")
+let goAway = document.querySelector("#goAway")
+let intervalId;
 
 initialize()
 function initialize(){
@@ -37,7 +39,7 @@ function initialize(){
     questionButton1.style.visibility = "hidden";
     correct.style.visibility = "hidden";
     end.style.visibility = "hidden";
-    
+    tryAgain.style.visibility ="hidden"
 }
 
 
@@ -70,12 +72,12 @@ function startTimer() {
     timer
     updateTimerDisplay(timer);
 
-    let intervalId = setInterval(() => {
+    intervalId = setInterval(() => {
         timer--;
         updateTimerDisplay(timer);
 
         if (timer <= 0) {
-            clearInterval(intervalId); 
+           
             quizEnd(); 
         }
     }, 1000); 
@@ -91,21 +93,19 @@ function startTimer() {
     }, 3000)}
    
    
-        // something about the initialize function or the event listener above is causing the current question to ++, needs re-structuring. added a blank line below for quick fix, will get dinged if left as is 
 let questions = [
                 {question:"Who named the city of Philadelphia?", answers:["William Penn", "George Washington", "Henry Hudson", "Leif Erikson"], correctAnswer:"William Penn"},
                  {question:"What year was Philadelphia established as the capital of the United States?", answers:["1776", "1780", "1790", "1800"], correctAnswer:"1790"},
                  {question:"Who was the first mayor of Philadelphia?", answers:["William Penn", "Griffith Jones", "Edward Shippen", "Humphrey Morrey"], correctAnswer:"Humphrey Morrey"},
                  {question:"Philadelphia is a combination of two words from which language?", answers:["French", "Greek", "Hebrew", "Dutch"], correctAnswer:"Greek"},   
-                 {question:"Philadelphia is known as 'The city of ?", answers:["Terrible drivers", "Brotherly love", "Cheesesteaks", "Lights"], correctAnswer:"Brotherly love"}   
+                 {question:"Philadelphia is known as 'The city of ?", answers:["Terrible drivers", "Brotherly love", "Cheesesteaks", "Lights"], correctAnswer:"Brotherly love"},
+                 {question:"", answers:[""], correctAnswer:""}
 ]
 
 
-// structure: questions, the global variable which is made up of several arrays, we take questions at the index 0, the first array, and grab the question obj there. that is the first line in this sequence. The first array contains 3 obj, question, answer and correct answer. to print the available answers, we look at the first array in the questions variable [0], then look for the obj answers, which is an array, and we look for the answers at given indexes 
-// renderQuestion();
-// have to call renderQuestion after questions variable is defined or text content displays empty
+
 function renderQuestion(){
-// going to have to change something about this so that it displays all available answer choices but one of them is linked to the correct answer because this function is being called to render the page for the next question, so currently choice 4 will always be the correct answer. maybe using random numbers, idk will have to think about this further
+
 h2El.textContent = (questions[currentQuestion].question);
 questionButton1.textContent = (questions[currentQuestion].answers[0]);
 questionButton2.textContent = (questions[currentQuestion].answers[1]);
@@ -115,14 +115,10 @@ questionButton4.textContent = (questions[currentQuestion].answers[3]);
 }
 
 buttons.addEventListener("click", function(event){
-  
+    
    
 if(event.target.matches("button")){
-    console.log("clicked")
-    // to check if answer clicked was correct answer check if value is the same as correct answer
-    
-    console.log("value: " + event.target.innerText);
-    console.log("Correct Answer: " + questions[currentQuestion].correctAnswer);
+   
     
    
 }
@@ -132,9 +128,9 @@ if (event.target.innerText === questions[currentQuestion].correctAnswer) {
     answer.textContent = "Correct"
     spanTime()
     playerScore +=10
-    console.log(playerScore)
     
-   localStorage.setItem("score:", JSON.stringify(playerScore))
+    
+   
 }
 if (event.target.innerText != questions[currentQuestion].correctAnswer)  {
     answer.style.visibility = "visible";
@@ -145,11 +141,17 @@ if (event.target.innerText != questions[currentQuestion].correctAnswer)  {
     
 
 }
+
+
 if (answer.style.visibility = "visible"){
     currentQuestion++;
     renderQuestion();
     
    
+}
+// the only way i could get the if statement below to work is if I added another blank question in the questions array. I tried a dozen solutions over several hours, and this works so i'm sticking with it.
+if (currentQuestion >= questions.length -1) {
+    quizEnd();
 }
 
 })
@@ -157,9 +159,10 @@ if (answer.style.visibility = "visible"){
 function timeCheck(){
     
 }
-// incriment the number for currentQuestion when button pressed to render the next question
+
 
 function quizEnd (){
+    clearInterval(intervalId); 
     initial.style.visibility = "hidden";
     quizDiv.style.visibility = "hidden";
     countdown.style.visibility = "hidden";
@@ -172,20 +175,22 @@ function quizEnd (){
     correct.style.visibility = "hidden";
     finalScore.textContent ="Your Score: " + playerScore;
     submit.textContent = "Submit"
-    let userName = document.getElementById('name').value
-    localStorage.setItem("Name:", JSON.stringify(userName))
-
+    tryAgain.style.visibility = "hidden"
+    finalScore.textContent = "Your Score: " + playerScore;
 }
 
 submit.addEventListener("click", function(event){
 
     if(event.target.matches("button")){
         let userName = document.getElementById('name').value
-        localStorage.setItem("Name:", JSON.stringify(userName))
+        let userScore = playerScore;
+        let highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+        highScores.push({name: userName, score: userScore});
+        localStorage.setItem('highScores', JSON.stringify(highScores))
         
     
+        displayHighScores();
     }
-    viewHighScores();
     
     })
     
@@ -193,12 +198,25 @@ submit.addEventListener("click", function(event){
   
    
         if(event.target.matches("button")){
-           viewHighScores();
+           displayHighScores();
             
            
         }})
         
-function viewHighScores(){
+
+tryAgain.addEventListener("click", function(event){
+
+    if(event.target.matches("button")){
+   
+    location.reload();
+    }
+    
+    
+    })
+
+    
+
+function displayHighScores() {
     initial.style.visibility = "hidden";
     quizDiv.style.visibility = "hidden";
     countdown.style.visibility = "hidden";
@@ -210,11 +228,29 @@ function viewHighScores(){
     questionButton1.style.visibility = "hidden";
     correct.style.visibility = "hidden";
     highScoreLink.style.visibility = "hidden";
-    scores.textContent = "High Scores"
-}
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+    scores.textContent = "High Scores";
+    tryAgain.style.visibility = "visible";
+    tryAgain.textContent = "Try again";
+    h3El.style.visibility = "hidden"
+    finalScore.style.visibility = "hidden"
+    nameBox.style.visibility = "hidden"
+    goAway.style.visibility ="hidden"
+    submit.style.visibility = "hidden"
+    let highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+    // Sorts the scores
+    highScores.sort((a, b) => b.score - a.score);
+
     
+    let highScoresList = document.getElementById('allScores');
+    highScoresList.innerHTML = '';
+// function for each element in an array
+    highScores.forEach((entry, index) => {
+        let listEl = document.createElement('li');
+        // entry is like the key
+        listEl.textContent = (index + 1) + '. ' + entry.name + ': ' + entry.score;
+        highScoresList.appendChild(listEl);
+    });
 }
 
 
@@ -222,4 +258,7 @@ for (let i = 0; i < array.length; i++) {
 
 
 
-// function to start timer countdown. Will have to change the h4 below to whichever html element the timer ends up being
+
+
+
+
